76a2506a681980c42fe3c2c5b7141447237d1d5e
sushantpawar87 <sushantpawar87@gmail.com>
2023-03-30 18:31:15 +0530

    all files added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name="census-income",
+      version = "0.0.1",
+      author= "sushant",
+      author_email="sushantpawar87@gmail.com",
+      packages=find_packages(),
+      install_requires = ["pandas","numpy","flask"]
+      )
